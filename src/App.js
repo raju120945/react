4f26d483b18107335be319881619a16dcdf4ff2a
Ramana <venkataramanar@threatlandscape.com>
2021-03-30 17:39:28 +0530

    new project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+
+  render() {
+    return (
+      <div>
+        sample react project
+      </div>
+    );
+  }
+}
+
+export default App;
