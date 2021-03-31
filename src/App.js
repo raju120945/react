0016a4fcc89c39401e2204d690d3ad8b32f61f85
Ramana <venkataramanar@threatlandscape.com>
2021-03-31 09:45:39 +0530

    sample api added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,39 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+	constructor() {
+		super();
+	}
 
-  render() {
-    return (
-      <div>
-        sample react project
+	componentDidMount() {
+		this.getDetails();
+	}
+
+	getDetails = () => {
+		letrequest = newXMLHttpRequest();
+		request.open("GET", `http://localhost:7777/chatRooms/${id}​​​​​​​​/messages`);
+		request.onreadystatechange = () => {
+			if (R.equals(request.readyState, 4)) {
+				const res = JSON.parse(request.response);
+				alert('apisuccess',res);
+				console.log('apirespone',res);
+			} else {
+				alert('apifailed',request);
+				console.log('apirespone',request);
+			}
+
+		};
+		request.setRequestHeader("Content-Type", "application/json");
+		request.send();
+	}
+
+	render() {
+		return (
+			<div>
+				sample react project
       </div>
-    );
-  }
+		);
+	}
 }
 
 export default App;
